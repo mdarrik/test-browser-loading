9c9ff2f6c312fb5064bc58810812176d543b6429
Darrik <30670444+mdarrik@users.noreply.github.com>
2021-05-07 20:02:22 -0700

    adding the basic site

---
+++ netlify/functions/delayed-response.js
@@ -0,0 +1,18 @@
+import Timers from 'timers-promises';
+export async handler() {
+await Timers.setTimeout(4000);
+return {statusCode: 200
+    body: `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is the page response</title>
+</head>
+<body>
+    <h1>This is the response page</h1>
+    <a href="/">Click here to go back home.</a>
+</body>
+</html>`}
+}
